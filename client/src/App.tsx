eb8bb714b9d2e9a13993c99d6d4dce4475a06d0c
Joaquin Acosta <joaquinacosta397@outlook.com>
2023-09-11 17:23:57 -0300

    added remove an complete task functionality

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,31 +1,57 @@
 import React from 'react'
 import { Todos } from './components/todos'
+import { type TodoId, type Todo as TodoType } from './types.d'
 
 const mockTodos = [
   {
     id: '1',
-    title: 'todo 1',
+    title: 'Rezar',
     completed: false
   },
   {
-    id: '23',
-    title: 'todo 2',
+    id: '2',
+    title: 'Dar las gracias al señor',
     completed: false
   },
   {
     id: '3',
-    title: 'todo 3',
+    title: 'Arrepentirme de mis pecados',
     completed: false
   }
 ]
 
 const App: React.FC = () => {
   const [ todos, setTodos ] = React.useState(mockTodos)
+
+  const handleRemove = ( { id }: TodoId) => {
+    const newTodos = todos.filter(todo => todo.id !== id)
+    setTodos(newTodos)
+  }
+
+  const handleCompleted = (
+    { id, completed }: Pick<TodoType, 'id' | 'completed'>) : void => {
+      const newTodos = todos.map( todo => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed
+          }
+        }
+        return todo
+      })
+
+      setTodos(newTodos)
+  }
+
   return (
-    <>
+    <div className='todoapp'>
       <h1>todo mvc</h1>
-      <Todos todos={todos}/>
-    </>
+      <Todos 
+        todos={todos}
+        onRemoveTodo={handleRemove}
+        onToggleCompleted={handleCompleted}
+      />
+    </div>
   )
 }
 
